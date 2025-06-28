53cfeae52bfea2f9aa2fbd3cd804b81ecb84983e
matinsaravani8013 <mtynsrawany@gmail.com>
2025-06-28 12:26:32 -0700

    create JoinUs components

--- src/components/JoinUs/FormJoin.jsx
+++ src/components/JoinUs/FormJoin.jsx
@@ -1,5 +1,7 @@
 import React from "react";
-
+import { FaRegUser } from "react-icons/fa";
+import { FaPhoneAlt } from "react-icons/fa";
+import { FaEnvelope } from "react-icons/fa";
 export default function FormJoin() {
   return (
     <form action="">
@@ -11,15 +13,68 @@ export default function FormJoin() {
           ثبت نام
         </h1>
       </div>
-      <div className="flex items-center justify-around">
-        <span className="w-full">
-          <label htmlFor=""></label>
-          <input className="w-[45%]" type="text" placeholder="شماره تلفن خود را وارد کنید" />
+      <div className="flex items-center justify-around mt-3">
+        <div>
+          <h1 className="text-right text-white font-bold text-[20px] mb-2">
+            شماره تلفن
+          </h1>
+          <span className="w-[264px] h-10 border-2 border-white flex items-center flex-row-reverse rounded-[6px]">
+            <label className="text-white ml-2 mr-2" htmlFor="">
+              <FaPhoneAlt />
+            </label>
+            <input
+              className="w-full text-right text-white outline-none"
+              type="text"
+              placeholder="شماره تلفن خود را وارد کنید"
+            />
+          </span>
+        </div>
+        <div>
+          <h1 className="text-right text-white font-bold text-[20px] mb-2">
+            نام و نام خانوادگی
+          </h1>
+          <span className="w-[264px] h-10 border-2 border-white flex items-center flex-row-reverse rounded-[6px]">
+            <label className="text-white ml-2 mr-2" htmlFor="">
+              <FaRegUser />
+            </label>
+            <input
+              className="w-full text-right text-white outline-none"
+              type="text"
+              placeholder="نام و نام خانوادگی خود را وارد کنید"
+            />
+          </span>
+        </div>
+      </div>
+      <div className="w-[565px] h-10 m-auto">
+        <h1 className="text-right text-white font-bold text-[20px] mb-2 mt-2 ">
+          ایمیل
+        </h1>
+        <span className=" flex flex-row-reverse items-center border-2 border-white rounded-[6px]  w-full h-full ">
+          <label className="text-white ml-2 mr-2" htmlFor="">
+            <FaEnvelope />
+          </label>
+          <input
+            className="w-full text-right text-white outline-none"
+            type="text"
+            placeholder="ایمیل خود را وارد کنید"
+          />
         </span>
-        <span className="w-full">
-          <label htmlFor=""></label>
-          <input className="w-[45%]" type="text" placeholder="نام و نام خانوادگی خود را وارد کنید" />
-        </span>
+        <button className="w-[565px] h-10 m-auto bg-red-400 text-white rounded-[5px] mt-3 cursor-pointer ">
+          ثبت نام
+        </button>
+        <div class="flex items-center my-4">
+          <div class="flex-grow border-t border-gray-300"></div>
+          <span class="mx-4 text-white">یا</span>
+          <div class="flex-grow border-t border-gray-300"></div>
+        </div>
+        <button class="w-full flex items-center justify-center gap-2 border cursor-pointer rounded-lg px-4 py-2 text-white hover:bg-gray-100 transition hover:text-black">
+          <span >ثبت‌نام با گوگل</span>
+          <img
+            src="https://www.svgrepo.com/show/475656/google-color.svg"
+            alt="Google"
+            className="w-5 h-5"
+          />
+        </button>
       </div>
     </form>
   );
--- src/components/JoinUs/JoinUs.jsx
+++ src/components/JoinUs/JoinUs.jsx
@@ -3,7 +3,7 @@ import FormJoin from "./FormJoin";
 
 export default function JoinUs() {
   return (
-    <section className="w-[90%] m-auto h-[400px] flex items-center flex-row-reverse justify-between border-2">
+    <section className="w-[90%] m-auto h-[400px] flex items-center flex-row-reverse justify-between ">
       <div className="w-[50%] mr-2">
         <h1 className="text-white font-bold text-right text-2xl mb-3">به جمع ما بپیوندید</h1>
         <p className="text-white text-right">
@@ -13,7 +13,7 @@ export default function JoinUs() {
           دوباره وارد صفحه‌ی خود شودید.
         </p>
       </div>
-      <div className="w-[50%] border-2 h-full">
+      <div className="w-[50%] h-full bg-red-1000 rounded-2xl">
         <FormJoin/>
       </div>
     </section>
